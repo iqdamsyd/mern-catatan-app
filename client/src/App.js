d3517a4e9e3c80ae2fad9d56c8e18acd923f894f
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-05 21:27:13 +0700

    Adding UserContext for storing user

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Landing from "./components/landing/Landing";
-// import Book from "./components/book/Book";
+import Book from "./components/book/Book";
+import UserContext from "./UserContext";
 
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
-      <Landing />
+      <UserContext.Provider value={{ user, setUser }}>
+        {user ? <Book /> : <Landing />}
+      </UserContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
