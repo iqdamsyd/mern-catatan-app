3557364c6e97da83bb98837738476215482c484c
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-15 20:14:37 +0700

    fix social media links

--- client/src/components/landing/Brand.js
+++ client/src/components/landing/Brand.js
@@ -16,25 +16,25 @@ const Brand = (props) => {
           The codes are available on my github pages.
         </div>
         <div className="social-media">
-          <a href="twitter.com">
+          <a href="https://www.twitter.com/iqdmsyd" target="blank">
             <img
               src={process.env.PUBLIC_URL + "/twitter-logo.png"}
               alt="twitter"
             />
           </a>
-          <a href="linkedin.com">
+          <a href="https://www.linkedin.com/in/iqdammusayyad" target="blank">
             <img
               src={process.env.PUBLIC_URL + "/linkedin-logo.png"}
               alt="twitter"
             />
           </a>
-          <a href="instagram.com">
+          <a href="https://www.instagram.com/iqdmsyd" target="blank">
             <img
               src={process.env.PUBLIC_URL + "/instagram-logo.png"}
               alt="twitter"
             />
           </a>
-          <a href="github.com">
+          <a href="https://www.github.com/iqdmsyd" target="blank">
             <img
               src={process.env.PUBLIC_URL + "/github-logo.png"}
               alt="twitter"
