86e664251d30928e72cd580251096d1dc6acabe8
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-15 19:55:59 +0700

    xs device top bar toggle

--- client/src/components/book/NoteSearch.js
+++ client/src/components/book/NoteSearch.js
@@ -1,8 +1,14 @@
 import React from "react";
+const xs_devices = 575.98;
+
 function NoteSearch(props) {
   return (
     <div className="NoteSearch">
-      <div className="container">
+      <div
+        className={
+          window.innerWidth <= xs_devices ? "container xs" : "container"
+        }
+      >
         <form onSubmit={props.handleSearchSubmit}>
           <input
             type="text"
@@ -12,6 +18,15 @@ function NoteSearch(props) {
             onChange={props.handleSearchChange}
           />
         </form>
+        {window.innerWidth <= xs_devices ? (
+          <img
+            className="btn"
+            src={process.env.PUBLIC_URL + "/xs_topbar.png"}
+            width="20px"
+            alt="img"
+            onClick={props.handleToggleSidebar}
+          />
+        ) : null}
       </div>
     </div>
   );
