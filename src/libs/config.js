e5dff1809190c3b53c886720f10c05951fcb01c7
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-16 14:07:08 +0700

    Version 1.0.0

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 const path = require("path");
 
-dotenv.config({
-  path: path.resolve("src", "environment", process.env.NODE_ENV + ".env"),
-});
+// dotenv.config({
+//   path: path.resolve("src", "environment", process.env.NODE_ENV + ".env"),
+// });
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
